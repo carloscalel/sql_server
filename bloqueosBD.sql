a6a2edbb3caf601e6b49d7cf5136773a7bb0ec72
Carlos Fernando Calel Lopez <49667963+carloscalel@users.noreply.github.com>
2025-04-15 14:20:06 -0600

    Actualizar bloqueosBD.sql

--- bloqueosBD.sql
+++ bloqueosBD.sql
@@ -1,28 +1,22 @@
 SELECT
-    r.session_id AS [Session ID],
+    r.session_id AS [Blocked Session ID],
     r.blocking_session_id AS [Blocking Session ID],
+    DB_NAME(r.database_id) AS [Database],
+    OBJECT_NAME(p.object_id, r.database_id) AS [Table Name],
+    s.login_name AS [User],
     r.status AS [Request Status],
     r.wait_type,
     r.wait_time,
-    r.wait_resource,
-    DB_NAME(r.database_id) AS [Database],
-    OBJECT_NAME(p.object_id) AS [Table Name],
     r.command,
     t.text AS [SQL Text],
-    s.login_name AS [Login Name],
     s.host_name,
     s.program_name,
-    r.start_time,
-    r.cpu_time,
-    r.reads,
-    r.writes
+    r.start_time
 FROM sys.dm_exec_requests r
-LEFT JOIN sys.dm_exec_sessions s ON r.session_id = s.session_id
+JOIN sys.dm_exec_sessions s ON r.session_id = s.session_id
 LEFT JOIN sys.dm_exec_connections c ON s.session_id = c.session_id
 OUTER APPLY sys.dm_exec_sql_text(r.sql_handle) t
-LEFT JOIN sys.dm_exec_requests req ON req.blocking_session_id = r.session_id
-LEFT JOIN sys.dm_exec_requests br ON r.blocking_session_id = br.session_id
-LEFT JOIN sys.dm_exec_query_plan(r.plan_handle) qp ON 1 = 1
-LEFT JOIN sys.partitions p ON r.resource_associated_entity_id = p.hobt_id
-WHERE r.blocking_session_id <> 0 -- Only blocked sessions
+LEFT JOIN sys.dm_os_waiting_tasks wt ON r.session_id = wt.session_id
+LEFT JOIN sys.partitions p ON wt.resource_address = p.hobt_id
+WHERE r.blocking_session_id <> 0
 ORDER BY r.start_time;
