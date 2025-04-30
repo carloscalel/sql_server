976be0fd9e98d16681d567e5e0457a7ee2dbf36c
Carlos Fernando Calel Lopez <49667963+carloscalel@users.noreply.github.com>
2025-04-30 11:54:56 -0600

    Actualizar transaccionesXsegundo.sql

--- transaccionesXsegundo.sql
+++ transaccionesXsegundo.sql
@@ -1,12 +1,17 @@
--- Script para monitorear las transacciones por segundo en SQL Server
+-- Script corregido para monitorear las transacciones por segundo en SQL Server
 
 DECLARE @ts_now BIGINT;
 DECLARE @ts_begin BIGINT;
 DECLARE @transactions_now BIGINT;
 DECLARE @transactions_begin BIGINT;
+DECLARE @cpu_ticks_per_ms DECIMAL(20, 5);
+
+-- Obtener el número de ticks de CPU por milisegundo
+SELECT @cpu_ticks_per_ms = cpu_ticks / CAST(ms_ticks AS DECIMAL(20, 5))
+FROM sys.dm_os_sys_info;
 
 -- Obtener la marca de tiempo inicial y el contador de transacciones
-SELECT @ts_begin = cpu_ticks / (cpu_ticks / ms_ticks),
+SELECT @ts_begin = cpu_ticks,
        @transactions_begin = cntr_value
 FROM sys.dm_os_performance_counters
 WHERE counter_name = 'Transactions/sec';
@@ -15,14 +20,14 @@ WHERE counter_name = 'Transactions/sec';
 WAITFOR DELAY '00:00:05'; -- Puedes ajustar este valor según necesites
 
 -- Obtener la marca de tiempo actual y el contador de transacciones actual
-SELECT @ts_now = cpu_ticks / (cpu_ticks / ms_ticks),
+SELECT @ts_now = cpu_ticks,
        @transactions_now = cntr_value
 FROM sys.dm_os_performance_counters
 WHERE counter_name = 'Transactions/sec';
 
 -- Calcular la diferencia en las marcas de tiempo (en segundos)
 DECLARE @elapsed_time_seconds DECIMAL(10, 2);
-SET @elapsed_time_seconds = CAST((@ts_now - @ts_begin) AS DECIMAL(20, 2)) / CAST((cpu_ticks / ms_ticks) AS DECIMAL(20, 2));
+SET @elapsed_time_seconds = CAST((@ts_now - @ts_begin) AS DECIMAL(20, 2)) / @cpu_ticks_per_ms / 1000;
 
 -- Calcular la diferencia en el número de transacciones
 DECLARE @transaction_difference INT;
