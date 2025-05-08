e99d9ada6a2324c37c77d230f4d9d837992c7710
Carlos Fernando Calel Lopez <49667963+carloscalel@users.noreply.github.com>
2025-05-08 11:12:19 -0600

    Actualizar bloqueosBD.sql

--- bloqueosBD.sql
+++ bloqueosBD.sql
@@ -1,22 +1,23 @@
 SELECT
-    r.session_id AS [Blocked Session ID],
-    r.blocking_session_id AS [Blocking Session ID],
-    DB_NAME(r.database_id) AS [Database],
-    OBJECT_NAME(p.object_id, r.database_id) AS [Table Name],
-    s.login_name AS [User],
+    r.session_id AS [Session ID],
     r.status AS [Request Status],
-    r.wait_type,
-    r.wait_time,
-    r.command,
+    r.blocking_session_id AS [Blocked By],
+    r.wait_type AS [Wait Type],
+    r.wait_time AS [Wait Time (ms)],
+    r.cpu_time AS [CPU Time (ms)],
+    r.total_elapsed_time AS [Elapsed Time (ms)],
     t.text AS [SQL Text],
-    s.host_name,
-    s.program_name,
-    r.start_time
-FROM sys.dm_exec_requests r
-JOIN sys.dm_exec_sessions s ON r.session_id = s.session_id
-LEFT JOIN sys.dm_exec_connections c ON s.session_id = c.session_id
-OUTER APPLY sys.dm_exec_sql_text(r.sql_handle) t
-LEFT JOIN sys.dm_os_waiting_tasks wt ON r.session_id = wt.session_id
-LEFT JOIN sys.partitions p ON wt.resource_address = p.hobt_id
-WHERE r.blocking_session_id <> 0
-ORDER BY r.start_time;
+    DB_NAME(r.database_id) AS [Database],
+    s.login_name AS [Login],
+    s.host_name AS [Host Name],
+    s.program_name AS [Program Name]
+FROM
+    sys.dm_exec_requests r
+JOIN
+    sys.dm_exec_sessions s ON r.session_id = s.session_id
+OUTER APPLY
+    sys.dm_exec_sql_text(r.sql_handle) t
+WHERE
+    r.blocking_session_id <> 0
+ORDER BY
+    r.total_elapsed_time DESC;
