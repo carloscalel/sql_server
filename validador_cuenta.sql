fd3d64e9545c8426188fc3979aadc47604fa3d13
Carlos Fernando Calel Lopez <49667963+carloscalel@users.noreply.github.com>
2025-05-16 15:16:59 -0600

    Actualizar validador_cuenta.sql

--- validador_cuenta.sql
+++ validador_cuenta.sql
@@ -1,24 +1,29 @@
--- Solo si tienes SQL Server 2016 o superior
-WITH Partes AS (
-    SELECT 
-        Cuenta,
-        LEFT(Cuenta, CHARINDEX('-', Cuenta) - 1) AS Parte1,
-        SUBSTRING(Cuenta, 
-                  CHARINDEX('-', Cuenta) + 1, 
-                  CHARINDEX('-', Cuenta, CHARINDEX('-', Cuenta) + 1) - CHARINDEX('-', Cuenta) - 1) AS Parte2,
-        RIGHT(Cuenta, LEN(Cuenta) - CHARINDEX('-', Cuenta, CHARINDEX('-', Cuenta) + 1)) AS Parte3
-    FROM Cuentas
-    WHERE 
-        Cuenta NOT LIKE '%[^0-9-]%' AND
-        LEN(Cuenta) - LEN(REPLACE(Cuenta, '-', '')) = 2 AND
-        CHARINDEX('-', Cuenta) > 1 AND
-        RIGHT(Cuenta, 1) <> '-'
-)
-SELECT 
-    Cuenta,
-    CASE
-        WHEN LEN(Parte1) <= 3 AND LEN(Parte2) <= 7 AND LEN(Parte3) = 1
-            THEN 'Formato válido'
-        ELSE 'Excede el formato XXX-XXXXXXX-X'
-    END AS Resultado
-FROM Partes;
+DECLARE @Cuenta VARCHAR(50) = '123-4567890-1';  -- Cambia esto para probar
+
+DECLARE @Parte1 VARCHAR(10), @Parte2 VARCHAR(10), @Parte3 VARCHAR(10);
+
+-- Solo si cumple estructura básica (2 guiones, no caracteres extraños)
+IF @Cuenta NOT LIKE '%[^0-9-]%' 
+   AND LEN(@Cuenta) - LEN(REPLACE(@Cuenta, '-', '')) = 2
+   AND CHARINDEX('-', @Cuenta) > 1
+   AND RIGHT(@Cuenta, 1) <> '-'
+BEGIN
+    -- Extraer partes
+    SET @Parte1 = LEFT(@Cuenta, CHARINDEX('-', @Cuenta) - 1);
+    
+    SET @Parte2 = SUBSTRING(
+        @Cuenta, 
+        CHARINDEX('-', @Cuenta) + 1, 
+        CHARINDEX('-', @Cuenta, CHARINDEX('-', @Cuenta) + 1) - CHARINDEX('-', @Cuenta) - 1
+    );
+    
+    SET @Parte3 = RIGHT(@Cuenta, LEN(@Cuenta) - CHARINDEX('-', @Cuenta, CHARINDEX('-', @Cuenta) + 1));
+
+    -- Validar tamaños de las partes
+    IF LEN(@Parte1) <= 3 AND LEN(@Parte2) <= 7 AND LEN(@Parte3) = 1
+        PRINT 'Formato válido';
+    ELSE
+        PRINT 'Excede el formato XXX-XXXXXXX-X';
+END
+ELSE
+    PRINT 'Formato inválido (estructura incorrecta)';
