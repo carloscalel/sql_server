41bd119742d20e6d6f3b167ffed452469797b180
Carlos Fernando Calel Lopez <49667963+carloscalel@users.noreply.github.com>
2025-05-28 17:38:38 -0600

    Actualizar Espacio discos.sql

--- Espacio discos.sql
+++ Espacio discos.sql
@@ -14,26 +14,12 @@ CREATE TABLE #Resultados (
     Sugerencia_SHRINK NVARCHAR(5)
 );
 
-DECLARE @sql NVARCHAR(MAX);
-
--- Recorremos todas las bases de datos que están ONLINE y no son TEMPDB
-DECLARE db_cursor CURSOR FOR
-SELECT name 
-FROM sys.databases 
-WHERE state_desc = 'ONLINE' AND database_id <> 2;
-
-DECLARE @dbname SYSNAME;
-OPEN db_cursor;
-FETCH NEXT FROM db_cursor INTO @dbname;
-
-WHILE @@FETCH_STATUS = 0
+EXEC sp_msforeachdb N'
+IF ''?'' NOT IN (''tempdb'')
 BEGIN
-    SET @sql = '
-    USE [' + QUOTENAME(@dbname) + '];
-
     INSERT INTO #Resultados
     SELECT 
-        DB_NAME() AS BaseDeDatos,
+        ''?'' AS BaseDeDatos,
         mf.name AS NombreArchivo,
         LEFT(mf.physical_name, 2) AS Disco,
         mf.physical_name AS RutaCompleta,
@@ -48,15 +34,9 @@ BEGIN
             THEN ''SÍ''
             ELSE ''NO''
         END AS Sugerencia_SHRINK
-    FROM sys.database_files mf;
-    ';
-    EXEC sp_executesql @sql;
-
-    FETCH NEXT FROM db_cursor INTO @dbname;
+    FROM [?].sys.database_files mf;
 END
-
-CLOSE db_cursor;
-DEALLOCATE db_cursor;
+';
 
 -- Mostrar los resultados
 SELECT * FROM #Resultados
