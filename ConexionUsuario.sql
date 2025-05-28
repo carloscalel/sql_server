711dcc6e6254bf9e33dcda75f544e8113409af0b
Carlos Fernando Calel Lopez <49667963+carloscalel@users.noreply.github.com>
2025-05-28 16:12:59 -0600

    Update ConexionUsuario.sql

--- ConexionUsuario.sql
+++ ConexionUsuario.sql
@@ -1,33 +1,30 @@
 SELECT * 
 FROM OPENQUERY(MiLinkedServer, '
-DECLARE @SQL NVARCHAR(MAX) = '''';
+-- Consulta para obtener información de usuarios de SQL Server
 
-DECLARE @ServerCollation NVARCHAR(128) = CAST(SERVERPROPERTY(''Collation'') AS NVARCHAR(128));
-DECLARE @ServerName NVARCHAR(128) = CAST(@@SERVERNAME AS NVARCHAR(128));
-
-SET @SQL += ''
+-- Información de usuarios a nivel de servidor
 SELECT 
-    ''''Server'''' AS Alcance,
-    sp.name COLLATE '' + @ServerCollation + '' AS NombreUsuario,
-    sp.type_desc COLLATE '' + @ServerCollation + '' AS TipoPrincipal,
-    sp.default_database_name COLLATE '' + @ServerCollation + '' AS BaseDatosPredeterminada,
+    ''Servidor'' AS Alcance,
+    sp.name AS NombreUsuario,
+    sp.type_desc AS TipoPrincipal,
+    sp.default_database_name AS BaseDatosPredeterminada,
     sp.create_date AS FechaCreacion,
     sp.modify_date AS FechaModificacion,
-    ISNULL(perm.class_desc COLLATE '' + @ServerCollation + '', '''''') AS ClasePermiso,
-    ISNULL(perm.permission_name COLLATE '' + @ServerCollation + '', '''''') AS Permiso,
-    ISNULL(perm.state_desc COLLATE '' + @ServerCollation + '', '''''') AS EstadoPermiso,
-    ISNULL(CAST(perm.major_id AS NVARCHAR) COLLATE '' + @ServerCollation + '', '''''') AS IdObjeto,
-    ISNULL(roles.RolePath COLLATE '' + @ServerCollation + '', '''''') AS RolesAsignados,
+    ISNULL(perm.class_desc, '''') AS ClasePermiso,
+    ISNULL(perm.permission_name, '''') AS Permiso,
+    ISNULL(perm.state_desc, '''') AS EstadoPermiso,
+    ISNULL(CAST(perm.major_id AS NVARCHAR), '''') AS IdObjeto,
+    ISNULL(roles.RolePath, '''') AS RolesAsignados,
     MAX(ses.login_time) AS UltimaConexion,
     MAX(CONVERT(VARCHAR(100), ses.login_time, 120)) AS UltimaConexionExacta,
     MAX(con.client_net_address) AS UltimaIP,
     MAX(ses.host_name) AS UltimoHostCliente,
     MAX(ses.program_name) AS UltimaAplicacionCliente,
-    '''''' + @ServerName + '''''' AS Servidor,
+    @@SERVERNAME AS Servidor,
     NULL AS BaseDatos,
     DATEDIFF(DAY, MAX(ses.login_time), GETDATE()) AS DiasSinConexion,
-    CASE WHEN sl.is_policy_checked = 1 THEN ''''Sí'''' ELSE ''''No'''' END AS PoliticaContraseña,
-    CASE WHEN sl.is_expiration_checked = 1 THEN ''''Sí'''' ELSE ''''No'''' END AS CaducidadContraseña
+    CASE WHEN sl.is_policy_checked = 1 THEN ''Sí'' ELSE ''No'' END AS PoliticaContraseña,
+    CASE WHEN sl.is_expiration_checked = 1 THEN ''Sí'' ELSE ''No'' END AS CaducidadContraseña
 FROM sys.server_principals sp
 LEFT JOIN sys.sql_logins sl ON sp.principal_id = sl.principal_id
 LEFT JOIN sys.dm_exec_sessions ses ON sp.sid = ses.security_id
@@ -36,76 +33,57 @@ LEFT JOIN sys.server_permissions perm ON sp.principal_id = perm.grantee_principa
 LEFT JOIN (
     SELECT member_principal_id,
            STUFF((
-                SELECT '''', '''' + sp2.name
+                SELECT '', '' + sp2.name
                 FROM sys.server_role_members srm2
                 JOIN sys.server_principals sp2 ON srm2.role_principal_id = sp2.principal_id
                 WHERE srm2.member_principal_id = srm1.member_principal_id
-                FOR XML PATH(''''''), TYPE).value(''.'', ''NVARCHAR(MAX)'')
-           , 1, 2, '''''') AS RolePath
+                FOR XML PATH(''''), TYPE).value('.', 'NVARCHAR(MAX)')
+           , 1, 2, '') AS RolePath
     FROM sys.server_role_members srm1
     GROUP BY member_principal_id
 ) roles ON sp.principal_id = roles.member_principal_id
-WHERE sp.type IN (''''S'''', ''''U'''', ''''G'''')
+WHERE sp.type IN (''S'', ''U'', ''G'')
 GROUP BY sp.name, sp.type_desc, sp.default_database_name, sp.create_date, sp.modify_date,
          perm.class_desc, perm.permission_name, perm.state_desc, perm.major_id, roles.RolePath, sp.sid, sl.is_policy_checked, sl.is_expiration_checked
-'';
 
-DECLARE @DBName NVARCHAR(255);
-DECLARE @DBCursor CURSOR;
+UNION ALL
 
-SET @DBCursor = CURSOR FOR 
-SELECT name FROM sys.databases WHERE database_id > 4;
-
-OPEN @DBCursor;
-FETCH NEXT FROM @DBCursor INTO @DBName;
-
-WHILE @@FETCH_STATUS = 0
-BEGIN
-    SET @SQL += ''
-    UNION ALL
-    SELECT 
-        ''''Database'''' AS Alcance,
-        dp.name COLLATE '' + @ServerCollation + '' AS NombreUsuario,
-        dp.type_desc COLLATE '' + @ServerCollation + '' AS TipoPrincipal,
-        NULL AS BaseDatosPredeterminada,
-        dp.create_date AS FechaCreacion,
-        dp.modify_date AS FechaModificacion,
-        ISNULL(perm.class_desc COLLATE '' + @ServerCollation + '', '''''') AS ClasePermiso,
-        ISNULL(perm.permission_name COLLATE '' + @ServerCollation + '', '''''') AS Permiso,
-        ISNULL(perm.state_desc COLLATE '' + @ServerCollation + '', '''''') AS EstadoPermiso,
-        ISNULL(CAST(perm.major_id AS NVARCHAR) COLLATE '' + @ServerCollation + '', '''''') AS IdObjeto,
-        ISNULL(roles.RolePath COLLATE '' + @ServerCollation + '', '''''') AS RolesAsignados,
-        NULL AS UltimaConexion,
-        NULL AS UltimaConexionExacta,
-        NULL AS UltimaIP,
-        NULL AS UltimoHostCliente,
-        NULL AS UltimaAplicacionCliente,
-        '''''' + @ServerName + '''''' AS Servidor,
-        '''''' + @DBName + '''''' AS BaseDatos,
-        NULL AS DiasSinConexion,
-        NULL AS PoliticaContraseña,
-        NULL AS CaducidadContraseña
-    FROM ['' + @DBName + ''].sys.database_principals dp
-    LEFT JOIN ['' + @DBName + ''].sys.database_permissions perm ON dp.principal_id = perm.grantee_principal_id
-    LEFT JOIN (
-        SELECT member_principal_id,
-               STUFF((
-                    SELECT '''', '''' + drp2.name
-                    FROM ['' + @DBName + ''].sys.database_role_members drm2
-                    JOIN ['' + @DBName + ''].sys.database_principals drp2 ON drm2.role_principal_id = drp2.principal_id
-                    WHERE drm2.member_principal_id = drm1.member_principal_id
-                    FOR XML PATH(''''''), TYPE).value(''.'', ''NVARCHAR(MAX)'')
-               , 1, 2, '''''') AS RolePath
-        FROM ['' + @DBName + ''].sys.database_role_members drm1
-        GROUP BY drm1.member_principal_id
-    ) roles ON dp.principal_id = roles.member_principal_id
-    WHERE dp.type IN (''''S'''', ''''U'''', ''''G'''', ''''E'''', ''''X'''')
-    '';
-    FETCH NEXT FROM @DBCursor INTO @DBName;
-END;
-
-CLOSE @DBCursor;
-DEALLOCATE @DBCursor;
-
-EXEC sp_executesql @SQL;
+-- Información de usuarios a nivel de base de datos
+SELECT 
+    ''Base de Datos'' AS Alcance,
+    dp.name AS NombreUsuario,
+    dp.type_desc AS TipoPrincipal,
+    NULL AS BaseDatosPredeterminada,
+    dp.create_date AS FechaCreacion,
+    dp.modify_date AS FechaModificacion,
+    ISNULL(perm.class_desc, '''') AS ClasePermiso,
+    ISNULL(perm.permission_name, '''') AS Permiso,
+    ISNULL(perm.state_desc, '''') AS EstadoPermiso,
+    ISNULL(CAST(perm.major_id AS NVARCHAR), '''') AS IdObjeto,
+    ISNULL(roles.RolePath, '''') AS RolesAsignados,
+    NULL AS UltimaConexion,
+    NULL AS UltimaConexionExacta,
+    NULL AS UltimaIP,
+    NULL AS UltimoHostCliente,
+    NULL AS UltimaAplicacionCliente,
+    @@SERVERNAME AS Servidor,
+    DB_NAME() AS BaseDatos,
+    NULL AS DiasSinConexion,
+    NULL AS PoliticaContraseña,
+    NULL AS CaducidadContraseña
+FROM sys.database_principals dp
+LEFT JOIN sys.database_permissions perm ON dp.principal_id = perm.grantee_principal_id
+LEFT JOIN (
+    SELECT member_principal_id,
+           STUFF((
+                SELECT '', '' + drp2.name
+                FROM sys.database_role_members drm2
+                JOIN sys.database_principals drp2 ON drm2.role_principal_id = drp2.principal_id
+                WHERE drm2.member_principal_id = drm1.member_principal_id
+                FOR XML PATH(''''), TYPE).value('.', 'NVARCHAR(MAX)')
+           , 1, 2, '') AS RolePath
+    FROM sys.database_role_members drm1
+    GROUP BY member_principal_id
+) roles ON dp.principal_id = roles.member_principal_id
+WHERE dp.type IN (''S'', ''U'', ''G'', ''E'', ''X'')
 ');
