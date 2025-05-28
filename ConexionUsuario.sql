a4e388c1d6012b513a549b3a2f81c14e92919b5b
Carlos Fernando Calel Lopez <49667963+carloscalel@users.noreply.github.com>
2025-05-28 10:11:43 -0600

    Update ConexionUsuario.sql

--- ConexionUsuario.sql
+++ ConexionUsuario.sql
@@ -15,32 +15,47 @@ WITH ServerRoleHierarchy AS (
     SELECT 
         srm.member_principal_id,
         srm.role_principal_id,
-        CAST(RolePath + ' > ' + sp_role.name AS NVARCHAR(MAX)) AS RolePath
+        CAST(srh.RolePath + ' > ' + sp_role.name AS NVARCHAR(MAX)) AS RolePath
     FROM sys.server_role_members srm
     JOIN sys.server_principals sp_role ON srm.role_principal_id = sp_role.principal_id
     JOIN ServerRoleHierarchy srh ON srm.member_principal_id = srh.role_principal_id
 )
+-- Combina los permisos y la jerarquía de roles
 SELECT 
     sp.name AS LoginName,
     sp.type_desc AS LoginType,
     sp.default_database_name AS DefaultDB,
     sp.create_date,
     sp.modify_date,
-    perm.class_desc,
-    perm.permission_name,
-    perm.state_desc AS PermissionState,
-    perm.major_id,
-    ISNULL(srh.RolePath, '') AS ServerRoles,
+    ISNULL(perm.class_desc, '') AS ClassDesc,
+    ISNULL(perm.permission_name, '') AS Permission,
+    ISNULL(perm.state_desc, '') AS PermissionState,
+    ISNULL(perm.major_id, 0) AS MajorID,
+    ISNULL(roles.RolePath, '') AS ServerRoles,
     MAX(ses.login_time) AS LastLoginTime
 FROM sys.server_principals sp
-LEFT JOIN sys.server_permissions perm
-    ON sp.principal_id = perm.grantee_principal_id
-LEFT JOIN ServerRoleHierarchy srh
-    ON sp.principal_id = srh.member_principal_id
 LEFT JOIN sys.dm_exec_sessions ses
     ON sp.sid = ses.security_id
-WHERE sp.type IN ('S', 'U', 'G') -- S=SQL Login, U=Windows Login, G=Windows Group
-GROUP BY sp.name, sp.type_desc, sp.default_database_name, sp.create_date, sp.modify_date, perm.class_desc, perm.permission_name, perm.state_desc, perm.major_id, srh.RolePath
+LEFT JOIN (
+    SELECT 
+        grantee_principal_id,
+        class_desc,
+        permission_name,
+        state_desc,
+        major_id
+    FROM sys.server_permissions
+) perm ON sp.principal_id = perm.grantee_principal_id
+LEFT JOIN (
+    SELECT 
+        member_principal_id,
+        STRING_AGG(RolePath, ', ') AS RolePath
+    FROM ServerRoleHierarchy
+    GROUP BY member_principal_id
+) roles ON sp.principal_id = roles.member_principal_id
+WHERE sp.type IN ('S', 'U', 'G')
+GROUP BY 
+    sp.name, sp.type_desc, sp.default_database_name, sp.create_date, sp.modify_date,
+    perm.class_desc, perm.permission_name, perm.state_desc, perm.major_id, roles.RolePath
 ORDER BY sp.name;
 
 -- ============================================
@@ -78,7 +93,7 @@ BEGIN
         SELECT 
             drm.member_principal_id,
             drm.role_principal_id,
-            CAST(RolePath + '' > '' + drp_role.name AS NVARCHAR(MAX)) AS RolePath
+            CAST(drh.RolePath + '' > '' + drp_role.name AS NVARCHAR(MAX)) AS RolePath
         FROM sys.database_role_members drm
         JOIN sys.database_principals drp_role ON drm.role_principal_id = drp_role.principal_id
         JOIN DBRoleHierarchy drh ON drm.member_principal_id = drh.role_principal_id
@@ -88,17 +103,29 @@ BEGIN
         dp.type_desc AS UserType,
         dp.create_date,
         dp.modify_date,
-        perm.class_desc,
-        perm.permission_name,
-        perm.state_desc AS PermissionState,
-        perm.major_id,
-        ISNULL(drh.RolePath, '''') AS DatabaseRoles
+        ISNULL(perm.class_desc, '''') AS ClassDesc,
+        ISNULL(perm.permission_name, '''') AS Permission,
+        ISNULL(perm.state_desc, '''') AS PermissionState,
+        ISNULL(perm.major_id, 0) AS MajorID,
+        ISNULL(roles.RolePath, '''') AS DatabaseRoles
     FROM sys.database_principals dp
-    LEFT JOIN sys.database_permissions perm
-        ON dp.principal_id = perm.grantee_principal_id
-    LEFT JOIN DBRoleHierarchy drh
-        ON dp.principal_id = drh.member_principal_id
-    WHERE dp.type IN (''S'', ''U'', ''G'', ''E'', ''X'') -- S=SQL user, U=Windows user, G=Windows group, E=External user, X=External group
+    LEFT JOIN (
+        SELECT 
+            grantee_principal_id,
+            class_desc,
+            permission_name,
+            state_desc,
+            major_id
+        FROM sys.database_permissions
+    ) perm ON dp.principal_id = perm.grantee_principal_id
+    LEFT JOIN (
+        SELECT 
+            member_principal_id,
+            STRING_AGG(RolePath, '', '') AS RolePath
+        FROM DBRoleHierarchy
+        GROUP BY member_principal_id
+    ) roles ON dp.principal_id = roles.member_principal_id
+    WHERE dp.type IN (''S'', ''U'', ''G'', ''E'', ''X'')
     ORDER BY dp.name;
     ';
 
