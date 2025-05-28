b2f7078776b2e18c9b8dc95ef1502c476edbcb60
Carlos Fernando Calel Lopez <49667963+carloscalel@users.noreply.github.com>
2025-05-28 10:30:32 -0600

    Update ConexionUsuario.sql

--- ConexionUsuario.sql
+++ ConexionUsuario.sql
@@ -1,7 +1,8 @@
 -- ============================================
--- A NIVEL DE INSTANCIA (Logins, permisos, roles anidados y última conexión)
+-- Consulta Unificada: Logins + Usuarios
 -- ============================================
 
+-- CTE para jerarquía de roles a nivel de instancia
 WITH ServerRoleHierarchy AS (
     SELECT 
         member_principal_id,
@@ -20,9 +21,11 @@ WITH ServerRoleHierarchy AS (
     JOIN sys.server_principals sp_role ON srm.role_principal_id = sp_role.principal_id
     JOIN ServerRoleHierarchy srh ON srm.member_principal_id = srh.role_principal_id
 )
+-- Consulta unificada
 SELECT 
-    sp.name AS LoginName,
-    sp.type_desc AS LoginType,
+    'Server' AS Scope,
+    sp.name AS PrincipalName,
+    sp.type_desc AS PrincipalType,
     sp.default_database_name AS DefaultDB,
     sp.create_date,
     sp.modify_date,
@@ -30,20 +33,14 @@ SELECT
     ISNULL(perm.permission_name, '') AS Permission,
     ISNULL(perm.state_desc, '') AS PermissionState,
     ISNULL(perm.major_id, 0) AS MajorID,
-    ISNULL(roles.RolePath, '') AS ServerRoles,
-    MAX(ses.login_time) AS LastLoginTime
+    ISNULL(roles.RolePath, '') AS Roles,
+    MAX(ses.login_time) AS LastLoginTime,
+    NULL AS DatabaseName
 FROM sys.server_principals sp
 LEFT JOIN sys.dm_exec_sessions ses
     ON sp.sid = ses.security_id
-LEFT JOIN (
-    SELECT 
-        grantee_principal_id,
-        class_desc,
-        permission_name,
-        state_desc,
-        major_id
-    FROM sys.server_permissions
-) perm ON sp.principal_id = perm.grantee_principal_id
+LEFT JOIN sys.server_permissions perm
+    ON sp.principal_id = perm.grantee_principal_id
 LEFT JOIN (
     SELECT 
         member_principal_id,
@@ -59,69 +56,56 @@ WHERE sp.type IN ('S', 'U', 'G')
 GROUP BY 
     sp.name, sp.type_desc, sp.default_database_name, sp.create_date, sp.modify_date,
     perm.class_desc, perm.permission_name, perm.state_desc, perm.major_id, roles.RolePath
-ORDER BY sp.name;
 
--- ============================================
--- A NIVEL DE BASE DE DATOS (Usuarios, permisos, roles anidados)
--- ============================================
+UNION ALL
 
+-- Consulta dinámica para cada base de datos
 DECLARE @DBName NVARCHAR(255);
+DECLARE @DynamicSQL NVARCHAR(MAX) = '';
 
 DECLARE db_cursor CURSOR FOR 
-SELECT name 
-FROM sys.databases
-WHERE database_id > 4 -- Omitir bases de sistema (opcional)
+SELECT name FROM sys.databases WHERE database_id > 4; -- Omitir bases de sistema
 
 OPEN db_cursor;
 FETCH NEXT FROM db_cursor INTO @DBName;
 
 WHILE @@FETCH_STATUS = 0
 BEGIN
-    PRINT 'Base de datos: ' + @DBName;
-    DECLARE @SQL NVARCHAR(MAX);
-
-    SET @SQL = '
-    USE [' + @DBName + '];
-
-    WITH DBRoleHierarchy AS (
-        SELECT 
-            drm.member_principal_id,
-            drm.role_principal_id,
-            CAST(drp_role.name AS NVARCHAR(MAX)) AS RolePath
-        FROM sys.database_role_members drm
-        JOIN sys.database_principals drp_role ON drm.role_principal_id = drp_role.principal_id
-
-        UNION ALL
-
-        SELECT 
-            drm.member_principal_id,
-            drm.role_principal_id,
-            CAST(drh.RolePath + '' > '' + drp_role.name AS NVARCHAR(MAX)) AS RolePath
-        FROM sys.database_role_members drm
-        JOIN sys.database_principals drp_role ON drm.role_principal_id = drp_role.principal_id
-        JOIN DBRoleHierarchy drh ON drm.member_principal_id = drh.role_principal_id
-    )
+    SET @DynamicSQL += '
     SELECT 
-        dp.name AS UserName,
-        dp.type_desc AS UserType,
+        ''Database'' AS Scope,
+        dp.name AS PrincipalName,
+        dp.type_desc AS PrincipalType,
+        NULL AS DefaultDB,
         dp.create_date,
         dp.modify_date,
         ISNULL(perm.class_desc, '''') AS ClassDesc,
         ISNULL(perm.permission_name, '''') AS Permission,
         ISNULL(perm.state_desc, '''') AS PermissionState,
         ISNULL(perm.major_id, 0) AS MajorID,
-        ISNULL(roles.RolePath, '''') AS DatabaseRoles
-    FROM sys.database_principals dp
+        ISNULL(roles.RolePath, '''') AS Roles,
+        NULL AS LastLoginTime,
+        ''' + @DBName + ''' AS DatabaseName
+    FROM [' + @DBName + '].sys.database_principals dp
+    LEFT JOIN [' + @DBName + '].sys.database_permissions perm
+        ON dp.principal_id = perm.grantee_principal_id
     LEFT JOIN (
-        SELECT 
-            grantee_principal_id,
-            class_desc,
-            permission_name,
-            state_desc,
-            major_id
-        FROM sys.database_permissions
-    ) perm ON dp.principal_id = perm.grantee_principal_id
-    LEFT JOIN (
+        WITH DBRoleHierarchy AS (
+            SELECT 
+                drm.member_principal_id,
+                drm.role_principal_id,
+                CAST(drp_role.name AS NVARCHAR(MAX)) AS RolePath
+            FROM [' + @DBName + '].sys.database_role_members drm
+            JOIN [' + @DBName + '].sys.database_principals drp_role ON drm.role_principal_id = drp_role.principal_id
+            UNION ALL
+            SELECT 
+                drm.member_principal_id,
+                drm.role_principal_id,
+                CAST(drh.RolePath + '' > '' + drp_role.name AS NVARCHAR(MAX)) AS RolePath
+            FROM [' + @DBName + '].sys.database_role_members drm
+            JOIN [' + @DBName + '].sys.database_principals drp_role ON drm.role_principal_id = drp_role.principal_id
+            JOIN DBRoleHierarchy drh ON drm.member_principal_id = drh.role_principal_id
+        )
         SELECT 
             member_principal_id,
             STUFF((
@@ -132,13 +116,13 @@ BEGIN
         FROM DBRoleHierarchy dr1
         GROUP BY member_principal_id
     ) roles ON dp.principal_id = roles.member_principal_id
-    WHERE dp.type IN (''S'', ''U'', ''G'', ''E'', ''X'')
-    ORDER BY dp.name;
+    WHERE dp.type IN (''S'', ''U'', ''G'', ''E'', ''X'') 
     ';
-
-    EXEC sp_executesql @SQL;
     FETCH NEXT FROM db_cursor INTO @DBName;
 END;
 
 CLOSE db_cursor;
 DEALLOCATE db_cursor;
+
+-- Ejecutar consulta dinámica
+EXEC sp_executesql @DynamicSQL;
