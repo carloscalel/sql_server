2cfd241377e89bf0abd04657bb52ad135fe57a66
Carlos Fernando Calel Lopez <49667963+carloscalel@users.noreply.github.com>
2025-05-28 17:34:10 -0600

    Actualizar Espacio discos.sql

--- Espacio discos.sql
+++ Espacio discos.sql
@@ -1,25 +1,63 @@
--- VERIFICA ESPACIOS / UBICACIONES DE ARCHIVOS DE BASES DE DATOS Y SUGIERE SHRINK
-EXEC sp_MSforeachdb '
-USE [?];
-SELECT
-    DB_NAME() AS BASE_DATOS,
-    df.name AS NOMBRE_ARCHIVO,
-    LEFT(df.physical_name, 1) AS DISCO,
-    df.physical_name AS RUTA_COMPLETA,
-    df.type_desc AS TIPO_ARCHIVO,
-    df.size / 128.0 AS TAMANIO_MB,
-    CAST(FILEPROPERTY(df.name, ''SpaceUsed'') AS INT) / 128.0 AS ESPACIO_USADO_MB,
-    df.size / 128.0 - CAST(FILEPROPERTY(df.name, ''SpaceUsed'') AS INT) / 128.0 AS ESPACIO_LIBRE_MB,
-    CASE 
-        WHEN df.type_desc = ''ROWS'' THEN df.size / 128.0 - CAST(FILEPROPERTY(df.name, ''SpaceUsed'') AS INT) / 128.0
-        ELSE 0 
-    END AS ESPACIO_COMPACTABLE_MB,
-    CASE 
-        WHEN df.type_desc = ''ROWS'' 
-             AND (df.size / 128.0 - CAST(FILEPROPERTY(df.name, ''SpaceUsed'') AS INT) / 128.0) > 500
-        THEN ''-- RECOMENDADO: DBCC SHRINKFILE(['' + df.name + ''], TRUNCATEONLY)''
-        ELSE ''-- No se recomienda compactar''
-    END AS SUGERENCIA_SHRINK
-FROM sys.database_files AS df
-WHERE df.type IN (0, 1);
-';
+IF OBJECT_ID('tempdb..#Resultados') IS NOT NULL
+    DROP TABLE #Resultados;
+
+CREATE TABLE #Resultados (
+    BaseDeDatos SYSNAME,
+    NombreArchivo SYSNAME,
+    Disco NVARCHAR(10),
+    RutaCompleta NVARCHAR(MAX),
+    TipoArchivo NVARCHAR(20),
+    Tamaño_Total_MB DECIMAL(18,2),
+    Espacio_Usado_MB DECIMAL(18,2),
+    Espacio_Libre_MB DECIMAL(18,2),
+    Espacio_Compactable_MB DECIMAL(18,2),
+    Sugerencia_SHRINK NVARCHAR(5)
+);
+
+DECLARE @sql NVARCHAR(MAX);
+
+-- Recorremos todas las bases de datos que están ONLINE y no son TEMPDB
+DECLARE db_cursor CURSOR FOR
+SELECT name 
+FROM sys.databases 
+WHERE state_desc = 'ONLINE' AND database_id <> 2;
+
+DECLARE @dbname SYSNAME;
+OPEN db_cursor;
+FETCH NEXT FROM db_cursor INTO @dbname;
+
+WHILE @@FETCH_STATUS = 0
+BEGIN
+    SET @sql = '
+    USE [' + QUOTENAME(@dbname) + '];
+
+    INSERT INTO #Resultados
+    SELECT 
+        DB_NAME() AS BaseDeDatos,
+        mf.name AS NombreArchivo,
+        LEFT(mf.physical_name, 2) AS Disco,
+        mf.physical_name AS RutaCompleta,
+        mf.type_desc AS TipoArchivo,
+        CAST(mf.size * 8 / 1024 AS DECIMAL(18,2)) AS Tamaño_Total_MB,
+        CAST(FILEPROPERTY(mf.name, ''SpaceUsed'') * 8 / 1024 AS DECIMAL(18,2)) AS Espacio_Usado_MB,
+        CAST((mf.size - FILEPROPERTY(mf.name, ''SpaceUsed'')) * 8 / 1024 AS DECIMAL(18,2)) AS Espacio_Libre_MB,
+        CAST((mf.size - FILEPROPERTY(mf.name, ''SpaceUsed'')) * 8 / 1024 AS DECIMAL(18,2)) AS Espacio_Compactable_MB,
+        CASE 
+            WHEN mf.size > 0 AND 
+                 (CAST(mf.size - FILEPROPERTY(mf.name, ''SpaceUsed'') AS FLOAT) / mf.size) > 0.3 
+            THEN ''SÍ''
+            ELSE ''NO''
+        END AS Sugerencia_SHRINK
+    FROM sys.database_files mf;
+    ';
+    EXEC sp_executesql @sql;
+
+    FETCH NEXT FROM db_cursor INTO @dbname;
+END
+
+CLOSE db_cursor;
+DEALLOCATE db_cursor;
+
+-- Mostrar los resultados
+SELECT * FROM #Resultados
+ORDER BY Disco, BaseDeDatos, TipoArchivo;
