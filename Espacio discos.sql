c70d6dc585bcb079f4d81eef480531fb6796773a
Carlos Fernando Calel Lopez <49667963+carloscalel@users.noreply.github.com>
2025-05-28 18:01:33 -0600

    Actualizar Espacio discos.sql

--- Espacio discos.sql
+++ Espacio discos.sql
@@ -1,4 +1,4 @@
--- Verifica espacio de archivos de bases de datos y sugiere SHRINK
+-- Verifica espacio de archivos de bases de datos y sugiere SHRINK, mostrando en MB y GB
 EXEC sp_MSforeachdb '
 USE [?];
 SELECT
@@ -7,19 +7,11 @@ SELECT
     LEFT(df.physical_name, 1) AS DISCO,
     df.physical_name AS RUTA_COMPLETA,
     df.type_desc AS TIPO_ARCHIVO,
+    -- Tamaño total
     CAST(df.size * 8.0 / 1024 AS DECIMAL(18,2)) AS TAMANO_MB,
+    CAST(df.size * 8.0 / 1024 / 1024 AS DECIMAL(18,2)) AS TAMANO_GB,
+    -- Espacio usado
     CAST(FILEPROPERTY(df.name, ''SpaceUsed'') * 8.0 / 1024 AS DECIMAL(18,2)) AS USADO_MB,
-    CAST((df.size - FILEPROPERTY(df.name, ''SpaceUsed'')) * 8.0 / 1024 AS DECIMAL(18,2)) AS LIBRE_MB,
-    CAST(CASE WHEN df.size > 0 THEN
-        (CAST(FILEPROPERTY(df.name, ''SpaceUsed'') AS FLOAT) / CAST(df.size AS FLOAT)) * 100 ELSE 0 END
-        AS DECIMAL(5,2)) AS PORCENTAJE_USADO,
-    CASE
-        WHEN df.type_desc = ''ROWS''
-             AND (df.size - FILEPROPERTY(df.name, ''SpaceUsed'')) * 8.0 / 1024 > 500
-            THEN ''RECOMENDADO: DBCC SHRINKFILE(['' + df.name + ''], TRUNCATEONLY)''
-        ELSE ''No se recomienda compactar''
-    END AS SUGERENCIA_SHRINK
-FROM sys.database_files AS df
-WHERE df.type IN (0,1)
-ORDER BY BASE_DATOS, DISCO, NOMBRE_ARCHIVO;
-'
+    CAST(FILEPROPERTY(df.name, ''SpaceUsed'') * 8.0 / 1024 / 1024 AS DECIMAL(18,2)) AS USADO_GB,
+    -- Espacio libre
+    CAST((df.size - FILEPROPERTY(df.name, ''SpaceUsed'')) * 8.0 / 1024 AS DECIMAL(18,2)) AS LIBRE
