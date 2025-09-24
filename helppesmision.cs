44c2bed15fa9b75c8f59e2526253e2553d7f53b9
Carlos Fernando Calel Lopez <49667963+carloscalel@users.noreply.github.com>
2025-09-23 20:14:11 -0600

    Actualizar helppesmision.cs

--- helppesmision.cs
+++ helppesmision.cs
@@ -1,178 +1,32 @@
-public static class PermissionHelper
-{
-    public static List<string> GetAccessibleModules()
-    {
-        var username = HttpContext.Current.User.Identity.Name;
-
-        using (var db = new BitacoraMvcDbEntities())
-        {
-            var user = db.Users.FirstOrDefault(u => u.UserName == username);
-            if (user == null) return new List<string>();
-
-            var userRoleIds = db.UserRoles
-                                .Where(ur => ur.UserId == user.UserId)
-                                .Select(ur => ur.RoleId)
-                                .ToList();
-
-            if (!userRoleIds.Any()) return new List<string>();
-
-            // Traer módulos únicos a los que tenga algún permiso
-            var modules = (from rp in db.RolePermissions
-                           join p in db.Permissions on rp.PermissionId equals p.PermissionId
-                           join m in db.Modules on p.ModuleId equals m.ModuleId
-                           where userRoleIds.Contains(rp.RoleId)
-                           select m.Name)
-                           .Distinct()
-                           .ToList();
-
-            return modules;
-        }
-    }
-}
-
-
-
-
-
 <ul class="nav navbar-nav">
-    @foreach (var module in BitacoraMvc.Helpers.PermissionHelper.GetAccessibleModules())
-    {
-        if (module == "Productos")
-        {
-            <li>@Html.ActionLink("Productos", "Index", "Productos")</li>
-        }
-        else if (module == "Ventas")
-        {
-            <li>@Html.ActionLink("Ventas", "Index", "Ventas")</li>
-        }
-        else if (module == "Solicitudes")
-        {
-            <li>@Html.ActionLink("Solicitudes", "Index", "SolicitudPermisos")</li>
-        }
-    }
-</ul>
-
-
-
-
-
--- 1. Crear un nuevo módulo
-INSERT INTO Modules (Name, Description)
-VALUES ('Reportes', 'Módulo de generación de reportes');
-
-DECLARE @NewModuleId INT = SCOPE_IDENTITY();
-
--- 2. Crear un permiso para el módulo (ejemplo: acceso completo)
-INSERT INTO Permissions (ModuleId, Action)
-VALUES (@NewModuleId, 'View');
-
-DECLARE @NewPermissionId INT = SCOPE_IDENTITY();
-
--- 3. Identificar el usuario al que queremos darle acceso
-DECLARE @UserName NVARCHAR(50) = 'carlos';  -- Cambia por el usuario real
-DECLARE @UserId INT;
-
-SELECT @UserId = UserId
-FROM Users
-WHERE UserName = @UserName;
-
--- 4. Ver el rol principal del usuario
-DECLARE @RoleId INT;
-
-SELECT TOP 1 @RoleId = RoleId
-FROM UserRoles
-WHERE UserId = @UserId;
-
--- Si no tiene rol, podrías asignarle uno
-IF @RoleId IS NULL
-BEGIN
-    -- Crear rol básico si no existe
-    INSERT INTO Roles (Name) VALUES ('DefaultRole');
-    SET @RoleId = SCOPE_IDENTITY();
-
-    INSERT INTO UserRoles (UserId, RoleId)
-    VALUES (@UserId, @RoleId);
-END
-
--- 5. Darle el permiso al rol del usuario
-IF NOT EXISTS (
-    SELECT 1 FROM RolePermissions 
-    WHERE RoleId = @RoleId AND PermissionId = @NewPermissionId
-)
-BEGIN
-    INSERT INTO RolePermissions (RoleId, PermissionId)
-    VALUES (@RoleId, @NewPermissionId);
-END
-
-
-
-
-@using BitacoraMvc.Helpers
-@using BitacoraMvc.Models
-
-@{
-    // Traer todas las acciones que el usuario puede ejecutar
-    var accessibleActions = PermissionHelper.GetAccessibleActions(); 
-}
-
-<ul class="nav navbar-nav">
-    @foreach (var module in BitacoraMvc.Helpers.PermissionHelper.GetAccessibleModules())
-    {
-        // Agrupar acciones por módulo
-        var actionsForModule = accessibleActions
-                               .Where(a => a.Module == module)
-                               .Select(a => a.Action)
-                               .Distinct()
-                               .ToList();
-
-        if (actionsForModule.Any())
-        {
-            <li class="nav-item dropdown">
-                <a class="nav-link dropdown-toggle" href="#" id="dropdown_@module" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                    @module
-                </a>
-                <ul class="dropdown-menu" aria-labelledby="dropdown_@module">
-                    @foreach (var action in actionsForModule)
-                    {
-                        <li>
-                            @* Ajusta los nombres de acciones a las URLs de tus controladores *@
-                            <a class="dropdown-item" href="@Url.Action(action, module)">
-                                @action
-                            </a>
-                        </li>
-                    }
-                </ul>
-            </li>
-        }
-    }
-</ul>
-
-
-
-<ul class="nav navbar-nav">
-    @foreach (var module in BitacoraMvc.Helpers.PermissionHelper.GetAccessibleModules())
-    {
-        <li class="nav-item dropdown">
-            <a class="nav-link dropdown-toggle" href="#" id="dropdown_@module" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                @module
-            </a>
-            <ul class="dropdown-menu" aria-labelledby="dropdown_@module">
-                @* Como en esta versión solo tenemos Index por módulo *@
+    <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="#" id="dropdownModules" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Módulos
+        </a>
+        <ul class="dropdown-menu" aria-labelledby="dropdownModules">
+            @foreach (var module in BitacoraMvc.Helpers.PermissionHelper.GetAccessibleModules())
+            {
+                @* Ajusta los enlaces según tu módulo *@
                 <li>
                     @if (module == "Productos")
                     {
-                        <a class="dropdown-item" href="@Url.Action("Index", "Productos")">Ir a Productos</a>
+                        <a class="dropdown-item" href="@Url.Action("Index", "Productos")">Productos</a>
                     }
                     else if (module == "Ventas")
                     {
-                        <a class="dropdown-item" href="@Url.Action("Index", "Ventas")">Ir a Ventas</a>
+                        <a class="dropdown-item" href="@Url.Action("Index", "Ventas")">Ventas</a>
                     }
                     else if (module == "Solicitudes")
                     {
-                        <a class="dropdown-item" href="@Url.Action("Index", "SolicitudPermisos")">Ir a Solicitudes</a>
+                        <a class="dropdown-item" href="@Url.Action("Index", "SolicitudPermisos")">Solicitudes</a>
+                    }
+                    else
+                    {
+                        @* Por si agregas módulos nuevos *@
+                        <a class="dropdown-item" href="@Url.Action("Index", module)">@module</a>
                     }
                 </li>
-            </ul>
-        </li>
-    }
+            }
+        </ul>
+    </li>
 </ul>
