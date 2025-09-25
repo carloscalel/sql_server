bff6168a8b4fb8fad62c8d6085e697eb1ca0b374
Carlos Fernando Calel Lopez <49667963+carloscalel@users.noreply.github.com>
2025-09-25 10:37:13 -0600

    Actualizar 1.sql

--- 1.sql
+++ 1.sql
@@ -1,26 +1,23 @@
-DECLARE 
-    @database_size NVARCHAR(50),
-    @unallocated_space NVARCHAR(50),
-    @reserved NVARCHAR(50),
-    @data NVARCHAR(50),
-    @index_size NVARCHAR(50),
-    @unused NVARCHAR(50);
+CREATE TABLE #DB_SpaceUsed (
+    database_name SYSNAME,
+    database_size NVARCHAR(50),
+    unallocated_space NVARCHAR(50)
+);
 
--- Ejecutar sp_spaceused con variables OUTPUT
-EXEC sp_spaceused 
-     @updateusage = N'TRUE', 
-     @database_size = @database_size OUTPUT, 
-     @unallocated_space = @unallocated_space OUTPUT, 
-     @reserved = @reserved OUTPUT, 
-     @data = @data OUTPUT, 
-     @index_size = @index_size OUTPUT, 
-     @unused = @unused OUTPUT;
+CREATE TABLE #DB_SpaceUsedDetail (
+    reserved NVARCHAR(50),
+    data NVARCHAR(50),
+    index_size NVARCHAR(50),
+    unused NVARCHAR(50)
+);
 
--- Ver resultados
-SELECT 
-    @database_size AS database_size,
-    @unallocated_space AS unallocated_space,
-    @reserved AS reserved,
-    @data AS data,
-    @index_size AS index_size,
-    @unused AS unused;
+-- Primer resultset
+INSERT INTO #DB_SpaceUsed (database_name, database_size, unallocated_space)
+EXEC sp_spaceused;
+
+-- Segundo resultset
+INSERT INTO #DB_SpaceUsedDetail (reserved, data, index_size, unused)
+EXEC sp_spaceused;
+
+SELECT * FROM #DB_SpaceUsed;
+SELECT * FROM #DB_SpaceUsedDetail;
