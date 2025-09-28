de80d239a61ce5868abccc0ad6c9155a72a912cb
Carlos Fernando Calel Lopez <49667963+carloscalel@users.noreply.github.com>
2025-09-28 14:29:47 -0600

    Actualizar 1.sql

--- 1.sql
+++ 1.sql
@@ -11,3 +11,7 @@ SELECT
     CAST(100.0 * (df.size - FILEPROPERTY(df.name, 'SpaceUsed')) / df.size AS DECIMAL(5,2)) AS FreeSpacePct -- % Libre
 FROM sys.database_files df
 WHERE df.type_desc = 'ROWS'; -- Solo MDF/NDF
+
+
+
+https://teams.microsoft.com/l/meetup-join/19%3ameeting_MjgyN2E2MDgtYzljMC00MjQxLWE2Y2ItNWY3Mjc0OGVkNmQ2%40thread.v2/0?context=%7b%22Tid%22%3a%224f1d8a3a-c21d-4415-9a3e-d743e350dc3c%22%2c%22Oid%22%3a%223ff34afa-9ec5-486d-bf1d-209101ca9113%22%7d
