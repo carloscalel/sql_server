07d30a034a56d37039499c2dbdedcde9189ba22f
Carlos Fernando Calel Lopez <49667963+carloscalel@users.noreply.github.com>
2025-09-29 07:47:31 -0600

    Actualizar 1.sql

--- 1.sql
+++ 1.sql
@@ -1,15 +1,15 @@
-<!-- Bootstrap 5 CSS -->
-<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" />
-
-<!-- DataTables + Bootstrap 5 CSS -->
-<link href="https://cdn.datatables.net/1.13.5/css/dataTables.bootstrap5.min.css" rel="stylesheet" />
-
-<!-- jQuery -->
-<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-
-<!-- Bootstrap 5 JS -->
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
-
-<!-- DataTables + Bootstrap 5 JS -->
-<script src="https://cdn.datatables.net/1.13.5/js/jquery.dataTables.min.js"></script>
-<script src="https://cdn.datatables.net/1.13.5/js/dataTables.bootstrap5.min.js"></script>
+@section Scripts {
+    <script>
+        $(function () {
+            $('#dataTable').DataTable({
+                "deferRender": true,   // Renderiza solo lo que se necesita
+                "processing": true,    // Muestra indicador de carga
+                "pageLength": 10,
+                "lengthMenu": [5, 10, 25, 50],
+                "language": {
+                    "url": "//cdn.datatables.net/plug-ins/1.13.5/i18n/es-ES.json"
+                }
+            });
+        });
+    </script>
+}
