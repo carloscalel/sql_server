f0b8b692147fa4a4b318eb7553328c2b7598b9ae
Carlos Fernando Calel Lopez <49667963+carloscalel@users.noreply.github.com>
2025-09-29 07:41:40 -0600

    Actualizar 1.sql

--- 1.sql
+++ 1.sql
@@ -1,66 +1,15 @@
-@model IEnumerable<dynamic> 
-@{
-    ViewBag.Title = ViewBag.Title ?? "Listado";
-}
+<!-- Bootstrap 5 CSS -->
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" />
 
-<h2>@ViewBag.Title</h2>
+<!-- DataTables + Bootstrap 5 CSS -->
+<link href="https://cdn.datatables.net/1.13.5/css/dataTables.bootstrap5.min.css" rel="stylesheet" />
 
-<!-- Tabla genérica con Bootstrap 5 -->
-<table id="dataTable" class="table table-striped table-bordered">
-    <thead class="table-dark">
-        <tr>
-            <!-- Encabezados dinámicos -->
-            @if (Model != null && Model.Any())
-            {
-                var props = Model.First().GetType().GetProperties();
-                foreach (var prop in props)
-                {
-                    <th>@prop.Name</th>
-                }
-                <th>Acciones</th>
-            }
-            else
-            {
-                <th>Sin datos disponibles</th>
-            }
-        </tr>
-    </thead>
-    <tbody>
-        @if (Model != null && Model.Any())
-        {
-            foreach (var item in Model)
-            {
-                <tr>
-                    @foreach (var prop in item.GetType().GetProperties())
-                    {
-                        <td>@prop.GetValue(item, null)</td>
-                    }
-                    <td>
-                        <!-- Botones de acción genéricos -->
-                        @Html.ActionLink("Editar", "Edit", new { id = item.GetType().GetProperty("Id")?.GetValue(item) }, new { @class = "btn btn-sm btn-warning" }) 
-                        @Html.ActionLink("Detalles", "Details", new { id = item.GetType().GetProperty("Id")?.GetValue(item) }, new { @class = "btn btn-sm btn-info" }) 
-                        @Html.ActionLink("Eliminar", "Delete", new { id = item.GetType().GetProperty("Id")?.GetValue(item) }, new { @class = "btn btn-sm btn-danger" })
-                    </td>
-                </tr>
-            }
-        }
-    </tbody>
-</table>
+<!-- jQuery -->
+<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
 
-@section Scripts {
-    <!-- DataTables + Bootstrap 5 configuración -->
-    <script>
-        $(document).ready(function () {
-            $('#dataTable').DataTable({
-                "language": {
-                    "url": "//cdn.datatables.net/plug-ins/1.13.5/i18n/es-ES.json"
-                },
-                "pageLength": 10,
-                "lengthMenu": [5, 10, 25, 50],
-                "ordering": true,
-                "searching": true,
-                "responsive": true
-            });
-        });
-    </script>
-}
+<!-- Bootstrap 5 JS -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
+
+<!-- DataTables + Bootstrap 5 JS -->
+<script src="https://cdn.datatables.net/1.13.5/js/jquery.dataTables.min.js"></script>
+<script src="https://cdn.datatables.net/1.13.5/js/dataTables.bootstrap5.min.js"></script>
