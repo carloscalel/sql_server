3ea627440bf9b03a21a28396cba69c11b76ba236
Carlos Fernando Calel Lopez <49667963+carloscalel@users.noreply.github.com>
2025-12-16 14:54:10 -0600

    Actualizar 1.sql

--- 1.sql
+++ 1.sql
@@ -4,26 +4,38 @@ SELECT
     s.name AS SchemaName,
     t.name AS TableName,
 
-    -- ✔ FILAS CORRECTAS
+    -- Filas correctas
+    SUM(CASE WHEN i.index_id IN (0,1) THEN p.rows ELSE 0 END) AS NumRows,
+
+    -- Reserved
+    SUM(a.total_pages) * 8 AS ReservedKB,
+
+    -- DATA (igual a sp_spaceused)
     SUM(
         CASE 
-            WHEN i.index_id IN (0,1) THEN p.rows 
+            WHEN a.type IN (1,2,3) THEN a.used_pages 
             ELSE 0 
         END
-    ) AS NumRows,
+    ) * 8 AS DataKB,
 
-    SUM(a.total_pages) * 8 AS ReservedKB,
-    SUM(a.data_pages) * 8 AS DataKB,
-    (SUM(a.used_pages) - SUM(a.data_pages)) * 8 AS IndexKB,
-    (SUM(a.total_pages) - SUM(a.used_pages)) * 8 AS UnusedKB
+    -- INDEX (igual a sp_spaceused)
+    SUM(
+        CASE 
+            WHEN a.type NOT IN (1,2,3) THEN a.used_pages 
+            ELSE 0 
+        END
+    ) * 8 AS IndexKB,
+
+    -- UNUSED
+    SUM(a.total_pages - a.used_pages) * 8 AS UnusedKB
 
 FROM sys.tables t
 JOIN sys.schemas s ON t.schema_id = s.schema_id
 JOIN sys.indexes i ON t.object_id = i.object_id
-JOIN sys.partitions p 
-    ON i.object_id = p.object_id 
+JOIN sys.partitions p
+    ON i.object_id = p.object_id
    AND i.index_id = p.index_id
-JOIN sys.allocation_units a 
+JOIN sys.allocation_units a
     ON p.partition_id = a.container_id
 WHERE t.is_ms_shipped = 0
 GROUP BY s.name, t.name;
