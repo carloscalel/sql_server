a722ccbd8bc5fe20037f3079f70f1df4535cd7e7
Carlos Fernando Calel Lopez <49667963+carloscalel@users.noreply.github.com>
2025-12-17 07:47:04 -0600

    Actualizar 1.sql

--- 1.sql
+++ 1.sql
@@ -1,41 +1,68 @@
-INSERT INTO ##TableSpaceAllDB
-SELECT
-    DB_NAME() AS DatabaseName,
-    s.name AS SchemaName,
-    t.name AS TableName,
+DECLARE 
+    @i INT = 1,
+    @max INT,
+    @DBName SYSNAME,
+    @SQL NVARCHAR(MAX);
 
-    -- ROWS correctos
-    SUM(CASE WHEN i.index_id IN (0,1) THEN p.rows ELSE 0 END) AS NumRows,
+SELECT @max = MAX(RowNum) FROM #Databases;
 
-    -- RESERVED
-    SUM(a.total_pages) * 8 AS ReservedKB,
+WHILE @i <= @max
+BEGIN
+    SELECT @DBName = DBName
+    FROM #Databases
+    WHERE RowNum = @i;
 
-    -- DATA (heap o clustered)
-    SUM(
-        CASE 
-            WHEN i.index_id IN (0,1) THEN a.used_pages
-            ELSE 0
-        END
-    ) * 8 AS DataKB,
+    SET @SQL = N'
+    USE ' + QUOTENAME(@DBName) + N';
 
-    -- INDEX (nonclustered)
-    SUM(
-        CASE 
-            WHEN i.index_id > 1 THEN a.used_pages
-            ELSE 0
-        END
-    ) * 8 AS IndexKB,
+    INSERT INTO ##TableSpaceAllDB
+    SELECT
+        DB_NAME() AS DatabaseName,
+        s.name AS SchemaName,
+        t.name AS TableName,
 
-    -- UNUSED
-    SUM(a.total_pages - a.used_pages) * 8 AS UnusedKB
+        -- ROWS (igual a sp_spaceused)
+        SUM(CASE WHEN i.index_id IN (0,1) THEN p.rows ELSE 0 END) AS NumRows,
 
-FROM sys.tables t
-JOIN sys.schemas s ON t.schema_id = s.schema_id
-JOIN sys.indexes i ON t.object_id = i.object_id
-JOIN sys.partitions p
-    ON i.object_id = p.object_id
-   AND i.index_id = p.index_id
-JOIN sys.allocation_units a
-    ON p.partition_id = a.container_id
-WHERE t.is_ms_shipped = 0
-GROUP BY s.name, t.name;
+        -- RESERVED
+        SUM(a.total_pages) * 8 AS ReservedKB,
+
+        -- DATA (heap + clustered)
+        SUM(
+            CASE 
+                WHEN i.index_id IN (0,1) 
+                THEN a.used_pages 
+                ELSE 0 
+            END
+        ) * 8 AS DataKB,
+
+        -- INDEX (nonclustered)
+        SUM(
+            CASE 
+                WHEN i.index_id > 1 
+                THEN a.used_pages 
+                ELSE 0 
+            END
+        ) * 8 AS IndexKB,
+
+        -- UNUSED
+        SUM(a.total_pages - a.used_pages) * 8 AS UnusedKB
+
+    FROM sys.tables t
+    JOIN sys.schemas s 
+        ON t.schema_id = s.schema_id
+    JOIN sys.indexes i 
+        ON t.object_id = i.object_id
+    JOIN sys.partitions p
+        ON i.object_id = p.object_id
+       AND i.index_id  = p.index_id
+    JOIN sys.allocation_units a
+        ON p.partition_id = a.container_id
+    WHERE t.is_ms_shipped = 0
+    GROUP BY s.name, t.name;
+    ';
+
+    EXEC sp_executesql @SQL;
+
+    SET @i += 1;
+END
